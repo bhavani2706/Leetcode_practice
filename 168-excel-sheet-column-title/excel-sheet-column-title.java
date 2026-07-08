e675674d31f028cc263ac01edfdcdf2cfd506bb3
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-08 12:22:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (76.79%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder ans=new StringBuilder();
+        while(n>0){
+            n--;
+            int r=n%26;
+            ans.append((char)(r+'A'));
+            n/=26;
+        }
+        return ans.reverse().toString();
+        
+    }
+}
