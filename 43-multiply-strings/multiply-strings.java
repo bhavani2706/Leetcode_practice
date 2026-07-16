387075760b4ca9e27516e1201161f11bfd302b22
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-16 18:15:08 +0530

    Time: 9 ms (10.85%) | Memory: 44.6 MB (11.85%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger one=new BigInteger(num1);
+        BigInteger two=new BigInteger(num2);
+        return one.multiply(two).toString();
+    }
+}
